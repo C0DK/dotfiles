739a1bbef261390a04532a511af4709cea4d88ca
Casper Weiss Bang <master@thecdk.net>
2019-09-29 09:23:31 +0200

    Derp search/replace fuckup

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,5 +1,5 @@
 
 
 msg=$(curl --silent --fail https://whatthecommit.com/index.txt)
-git -C ~/.dotfiles aacm "$msg"
-git -C ~/Documents aacm "$msg"
+git -C ~/.dotfiles aacmp "$msg"
+git -C ~/Documents aacmp "$msg"
