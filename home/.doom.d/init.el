60b9d961493c2b5c425d829175ab67c2e9d8bb6c
Casper Weiss Bang <master@thecdk.net>
2020-02-13 09:17:31 +0100

    Some bugs fixed

--- home/.doom.d/init.el
+++ home/.doom.d/init.el
@@ -114,7 +114,7 @@
        ;;erlang            ; an elegant language for a more civilized age
        ;;ess               ; emacs speaks statistics
        ;;faust             ; dsp, but you get to keep your soul
-       ;;fsharp           ; ML stands for Microsoft's Language
+       fsharp           ; ML stands for Microsoft's Language
        go                ; the hipster dialect
        ;;(haskell +dante)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
