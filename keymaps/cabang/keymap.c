04ec02110c29fdd624340cae89da6f7683609354
Casper Weiss Bang <master@thecdk.net>
2020-03-26 14:13:39 +0100

    Too lazy to write descriptive message

--- keymaps/cabang/keymap.c
+++ keymaps/cabang/keymap.c
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, RESET,   DEBUG,   _______, _______,     _______, _______, TERM_ON,     TERM_OFF, _______, _______, KC_DEL,  \
   _______, _______, MU_MOD,  AU_ON,   AU_OFF,      AG_NORM, AG_SWAP, _______,     _______,  _______, RGB_COF, _______, \
   _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,      MI_ON,   MI_OFF,  RGB_TOG,     RGB_MOD,  RGB_M_P, RGB_CON, _______, \
-  _______, _______, _______, _______, TO(_QWERTY), _______, _______, TO(_QWERTY), _______,  _______, _______, _______  \
+  _______, _______, AG_SWAP, AG_NORM, TO(_QWERTY), _______, _______, TO(_QWERTY), _______,  _______, _______, _______  \
 )
 
 
