55bc3717b760d64cc5f5727c35b30b84951cd1b1
Casper Weiss Bang <master@thecdk.net>
2020-07-19 17:40:10 +0200

    Either Hot Shit or Total Bollocks

---
+++ slock/config.def.h
@@ -0,0 +1,28 @@
+/* user and group to drop privileges to */
+static const char *user  = "cdk";
+static const char *group = "cdk";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#005577",   /* during input */
+	[FAILED] = "#CC3333",   /* wrong password */
+	[TEXT] =   "#ffffff",   /* text  */
+};
+
+ResourcePref resources[] = {
+		{ "color0",       STRING,  &colorname[INIT] },
+		{ "color8",       STRING,  &colorname[INPUT] },
+		{ "color1",       STRING,  &colorname[FAILED] },
+		{ "foreground",   STRING,  &colorname[TEXT] },
+};
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/* default message */
+static const char * message = "PLEASE AUTHENTICATE";
+
+/* text color */
+static const char * text_color = &colorname[TEXT];
+
+/* text size (must be a valid size) */
+static const char * font_name = "10x20";
