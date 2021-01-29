7fa9cc754a56773cfffbb26cbb28720e04177426
Casper Weiss Bang <master@thecdk.net>
2021-01-29 20:38:51 +0100

    NSA backdoor - ignore

---
+++ home/bin/clockify_hours.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+from dateutil.relativedelta import relativedelta, MO
+from functools import lru_cache
+from decimal import Decimal
+from dataclasses import dataclass
+from typing import List, Tuple
+
+from datetime import datetime
+
+import requests
+import os
+
+WORKSPACE_ID = os.getenv("WORKSPACE_ID")
+CLOCKIFY_API_KEY = os.getenv("CLOCKIFY_API_KEY")
+BASE_URL = "https://api.clockify.me/api/v1"
+REPORTS_BASE_URL = "https://reports.api.clockify.me/v1"
+
+WORKSPACE_URL = BASE_URL + "/workspaces"
+DETAILED_REPORT_URL = f"{REPORTS_BASE_URL}/workspaces/{WORKSPACE_ID}/reports/detailed"
+
+
+def is_respose_success(response) -> bool:
+    return 200 <= response.status_code < 300
+
+
+def get_json(url):
+
+    response = requests.get(url, headers={"X-Api-Key": CLOCKIFY_API_KEY})
+
+    assert is_respose_success(response)
+
+    return response.json()
+
+
+def post_json(url, body):
+    response = requests.post(
+        url,
+        headers={
+            "X-Api-Key": CLOCKIFY_API_KEY,
+        },
+        json=body,
+    )
+
+    assert is_respose_success(
+        response
+    ), f"Response failed with {response.status_code=} and {response.text=}"
+
+    return response.json()
+
+
+def get_workspace_ids() -> List[Tuple[str, str]]:
+    assert CLOCKIFY_API_KEY is not None
+
+    workspaces = get_json(WORKSPACE_URL)
+    return [(workspace["name"], workspace["id"]) for workspace in workspaces]
+
+
+@dataclass()
+class DateRange:
+    start: datetime
+    end: datetime
+
+
+def get_detailed_report(date_range: DateRange):
+    return post_json(
+        DETAILED_REPORT_URL,
+        {
+            "dateRangeStart": date_range.start.isoformat(),
+            "dateRangeEnd": date_range.end.isoformat(),
+            "detailedFilter": {
+                "page": 1,
+                "pageSize": 50,
+            },
+        },
+    )
+
+def at_midnight(dt: datetime) -> datetime:
+  return datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+
+def this_month() -> DateRange:
+    return DateRange(
+        start=at_midnight(datetime.now()).replace(day=1),
+        end=datetime.now(),
+    )
+
+def this_week() -> DateRange:
+    return DateRange(
+        start=at_midnight(datetime.now()) - relativedelta(weekday=MO),
+        end=datetime.now(),
+    )
+
+@lru_cache(maxsize=32)
+def get_detailed_report_month():
+    return get_detailed_report(this_month())
+
+
+def total_hours(date_range: DateRange):
+    return get_detailed_report(date_range)["totals"][0]["totalBillableTime"]
+
+
+def total_wage(date_range: DateRange):
+    return Decimal(get_detailed_report(date_range)["totals"][0]["totalAmount"]) / 100
+
+
+def in_dkk(amount: Decimal) -> str:
+    return f"{amount:,.2f} DKK"
+
+
+if __name__ == "__main__":
+    print(f"{in_dkk(total_wage(this_month()))=}")
+    print(f"{in_dkk(total_wage(this_week()))=}")
--- home/bin/spotify_status.py
+++ home/bin/spotify_status.py
@@ -5,55 +5,44 @@ import dbus
 import argparse
 
 parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--trunclen", type=int, metavar="trunclen")
 parser.add_argument(
-    '-t',
-    '--trunclen',
-    type=int,
-    metavar='trunclen'
+    "-f", "--format", type=str, metavar="custom format", dest="custom_format"
 )
 parser.add_argument(
-    '-f',
-    '--format',
-    type=str,
-    metavar='custom format',
-    dest='custom_format'
+    "-p", "--playpause", type=str, metavar="play-pause indicator", dest="play_pause"
 )
 parser.add_argument(
-    '-p',
-    '--playpause',
+    "--font",
     type=str,
-    metavar='play-pause indicator',
-    dest='play_pause'
+    metavar="the index of the font to use for the main label",
+    dest="font",
 )
 parser.add_argument(
-    '--font',
+    "--playpause-font",
     type=str,
-    metavar='the index of the font to use for the main label',
-    dest='font'
-)
-parser.add_argument(
-    '--playpause-font',
-    type=str,
-    metavar='the index of the font to use to display the playpause indicator',
-    dest='play_pause_font'
+    metavar="the index of the font to use to display the playpause indicator",
+    dest="play_pause_font",
 )
 
 
 args = parser.parse_args()
+
 
 def fix_string(string):
     # corrects encoding for the python version used
     if sys.version_info.major == 3:
         return string
     else:
-        return string.encode('utf-8')
+        return string.encode("utf-8")
+
 
 # Default parameters
-output = fix_string(u'{play_pause} {artist}: {song}')
+output = fix_string(u"{play_pause} {artist}: {song}")
 trunclen = 60
-play_pause = fix_string(u'\u25B6,\u23F8') # first character is play, second is paused
+play_pause = fix_string(u"\u25B6,\u23F8")  # first character is play, second is paused
 
-label_with_font = '%{{T{font}}}{label}%{{T-}}'
+label_with_font = "%{{T{font}}}{label}%{{T-}}"
 font = args.font
 play_pause_font = args.play_pause_font
 
@@ -68,25 +57,21 @@ if args.play_pause is not None:
 try:
     session_bus = dbus.SessionBus()
     spotify_bus = session_bus.get_object(
-        'org.mpris.MediaPlayer2.spotify',
-        '/org/mpris/MediaPlayer2'
+        "org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2"
     )
 
-    spotify_properties = dbus.Interface(
-        spotify_bus,
-        'org.freedesktop.DBus.Properties'
-    )
+    spotify_properties = dbus.Interface(spotify_bus, "org.freedesktop.DBus.Properties")
 
-    metadata = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
-    status = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus')
+    metadata = spotify_properties.Get("org.mpris.MediaPlayer2.Player", "Metadata")
+    status = spotify_properties.Get("org.mpris.MediaPlayer2.Player", "PlaybackStatus")
 
     # Handle play/pause label
 
-    play_pause = play_pause.split(',')
+    play_pause = play_pause.split(",")
 
-    if status == 'Playing':
+    if status == "Playing":
         play_pause = play_pause[0]
-    elif status == 'Paused':
+    elif status == "Paused":
         play_pause = play_pause[1]
     else:
         play_pause = str()
@@ -95,28 +80,32 @@ try:
         play_pause = label_with_font.format(font=play_pause_font, label=play_pause)
 
     # Handle main label
-    artist = fix_string(metadata['xesam:artist'][0]) if metadata['xesam:artist'] else ''
-    song = fix_string(metadata['xesam:title']) if metadata['xesam:title'] else ''
-    album = fix_string(metadata['xesam:album']) if metadata['xesam:album'] else ''
+    artist = fix_string(metadata["xesam:artist"][0]) if metadata["xesam:artist"] else ""
+    song = fix_string(metadata["xesam:title"]) if metadata["xesam:title"] else ""
+    album = fix_string(metadata["xesam:album"]) if metadata["xesam:album"] else ""
 
     if not artist and not song and not album:
-        print('')
+        print("")
     else:
         if len(song) > trunclen:
             song = song[0:trunclen]
-            song += '...'
-            if ('(' in song) and (')' not in song):
-                song += ')'
+            song += "..."
+            if ("(" in song) and (")" not in song):
+                song += ")"
 
         if font:
             artist = label_with_font.format(font=font, label=artist)
             song = label_with_font.format(font=font, label=song)
             album = label_with_font.format(font=font, label=album)
 
-        print(output.format(artist=artist or album, song=song, play_pause=play_pause, album=album))
+        print(
+            output.format(
+                artist=artist or album, song=song, play_pause=play_pause, album=album
+            )
+        )
 
 except Exception as e:
     if isinstance(e, dbus.exceptions.DBusException):
-        print('')
+        print("")
     else:
         print(e)
