20edce3eea63f2280222504a779c3113474d834a
Casper Weiss Bang <master@thecdk.net>
2021-02-09 11:50:03 +0100

    Not one conflict, today was a good day.

--- doom/init.el
+++ doom/init.el
@@ -159,6 +159,7 @@
        plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        (python            ; beautiful is better than ugly
+        +pyenv
         +lsp)
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
